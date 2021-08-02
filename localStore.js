853e4368f8d9d8ec9d112cd04941e6ac2dd14ee4
saqebkhan <saqebk619@gmail.com>
2021-08-03 00:15:40 +0530

    Create localStore.js

---
+++ localStore.js
@@ -0,0 +1,9 @@
+// window.localStorage.setItem("pata","nizam colony")
+
+// console.log(localStorage("pata"))
+
+
+
+// window.sessionStorage.setItem("colony","naka")
+
+// console.log(sessionStorage("colony"))
