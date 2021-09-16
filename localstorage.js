fe99449e5bc1871c21377c0d4e71f0667fcbc72c
saqebkhan <saqebk619@gmail.com>
2021-09-16 18:06:57 +0530

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -1,4 +1,4 @@
-// window.localStorage.setItem('email','saqebk619@gmail.com')
-// window.localStorage.setItem('email','saqebk619@gmail.com')
+localStorage.setItem('email','saqebk619@gmail.com')
+localStorage.setItem('email','saqebk619@gmail.com')
 
-// console.log(localStorage.setItem('email'))
+console.log(localStorage.setItem('email'))
